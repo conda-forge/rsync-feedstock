50796e90e126d6e56cb653b8edeb193fbf2a1fe2
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-21 14:09:41 +0200

    Build for Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,7 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
     EXTRA_CONFIGURE_ARGS="--disable-simd"
 fi
 ./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
+[[ "$target_platform" == "win-64" ]] && patch_libtool
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
     make check
