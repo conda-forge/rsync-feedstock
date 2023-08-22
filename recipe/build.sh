d0cec52e9321e31a9679838525e5d0e3934a7f9b
Uwe L. Korn <uwe.korn@quantco.com>
2023-08-22 09:23:58 +0200

    Output config.log on error

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ fi
 if [[ "${target_platform}" == "osx-arm64" ]]; then
     EXTRA_CONFIGURE_ARGS="--disable-simd --build=x86_64-pc-cygwin"
 fi
-./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
+./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-} || (cat config.log; exit 1)
 [[ "$target_platform" == "win-64" ]] && patch_libtool
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
