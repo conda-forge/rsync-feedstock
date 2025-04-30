1cf0c539eb28491d63b35eae5d4906e3e47f6202
Uwe L. Korn <uwe.korn@quantco.com>
2025-04-30 21:54:19 +0200

    Omit tests with emulation

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,8 @@ if [[ "${target_platform}" == "osx-arm64" ]]; then
 fi
 ./configure --prefix=$PREFIX --without-included-zlib --without-included-popt ${EXTRA_CONFIGURE_ARGS:-}
 make -j${CPU_COUNT}
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+    # Don't run tests with emulation as they will be flaky due to the slow timings.
     make check
 fi
 make install
